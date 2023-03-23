2e403dc83cc4a991c3cfcf5eb09c9af030107af3
AliAhsan12 <125795058+AliAhsan12@users.noreply.github.com>
2023-03-23 15:38:49 +0500

    Delete 3_tier_aws_network_vpc directory

--- 3_tier_aws_network_vpc/myWebsite-ecs/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket  = "enter your bucket name"
-    key     = "myWebsite-ecs.tfstate"
-    region  = "us-east-1"
-    profile = "enter your profile name"
-  }
-}
